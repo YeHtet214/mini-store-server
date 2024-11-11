bbdb6aa62a1074defd3d1ddac8657cf23359dea0
yehtet <yhtet1934@gmail.com>
2024-11-11 13:32:30 +0800

    updated-greetfunction

--- api/greet.js
+++ api/greet.js
@@ -1,5 +1,15 @@
-export default handler = async (req, res) => {
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // or your specific origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'GET') {
-        res.json("Hello World!");
+        try {
+            return res.status(200).json("Hello World!");
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ msg: 'Error Greeting' });
+        }
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
     }
 }
