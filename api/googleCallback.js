bac71f85822fb8306038858ed57e325052e64617
yehtet <yhtet1934@gmail.com>
2024-11-11 22:09:19 +0800

    update-cross-origins

--- api/googleCallback.js
+++ api/googleCallback.js
@@ -1,8 +1,10 @@
 import passport from "../auth/passport.js";
 import * as UserService from "../services/userService.js";
 
+const origins = ["https://mini-store-omega.vercel.app", "http://localhost:5173"];
+
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', origins);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
