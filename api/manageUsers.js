aa1009f8cfdc16bf426019113a450e17c64adcce
yehtet <yhtet1934@gmail.com>
2024-11-11 15:51:10 +0800

    remove-extra-users-function

--- api/manageUsers.js
+++ api/manageUsers.js
@@ -4,7 +4,16 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*'); // or your specific origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'POST') {
+
+    if (req.method === 'GET') {
+        try {
+            const users = await UserService.getAllUsers();
+            return res.status(200).json(users);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({msg: 'Error getting users'});
+        }
+    } else if (req.method === 'POST') {
         const userData = req.body;
 
         try {
