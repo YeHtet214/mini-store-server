e27f574c67dfa76b2fbae260bf0771b14920e526
yehtet <yhtet1934@gmail.com>
2024-11-12 20:28:34 +0800

    test-google

--- api/google.js
+++ api/google.js
@@ -1,15 +1,16 @@
 import passport from "../auth/passport.js";
 
-const origins = ["https://mini-store-omega.vercel.app", "http://localhost:5173/auth/login"];
+
 
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', origins.join(", "));
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // CORS headers (for frontend communication only)
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    // Handle OPTIONS method for CORS preflight request
+    // Handle preflight request for CORS
     if (req.method === 'OPTIONS') {
-        return res.status(200).end(); // Respond OK to preflight
+        return res.status(200).end();
     }
 
     if (req.method === "GET") {
