07b4bc78651941f9b6dd2b6347e92ed9b4f2aa64
yehtet <yhtet1934@gmail.com>
2024-11-14 21:01:15 +0800

    fix typo error in header

--- api/manageCartItems.js
+++ api/manageCartItems.js
@@ -1,8 +1,8 @@
 import * as CartService from "../services/cartService.js";
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // or your specific origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     const { method } = req;
--- api/userProfile.js
+++ api/userProfile.js
@@ -3,7 +3,7 @@ import {verifyToken} from "../auth/auth.js";
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*'); // or your specific origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     const { method } = req;
