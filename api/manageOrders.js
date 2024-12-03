d6301b722d60e1641ed9ef2d8154b151cbdf3947
yehtet <yhtet1934@gmail.com>
2024-12-03 20:28:00 +0800

    get all order items changed database query

--- api/manageOrders.js
+++ api/manageOrders.js
@@ -12,8 +12,7 @@ export default async function handler(req, res) {
     }
 
     if (method === 'GET') {
-        const { userId } = req.params.userId;
-        const filter = req.query.filter;
+        const { userId,filter } = req.query.;
         console.log("userid in order getting", userId)
         console.log("query", req.query);
         console.log("params", req.params);
